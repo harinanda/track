c16a937459981aec5d55c6f2c02fc7054db3d9d8
root <root@server1.tracktrucks.in>
2013-02-28 22:45:15 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracktrucks::Application.config.secret_token = '8056fdf29ec154a2744793c65ba7c53c54fb54c8cd17de2133d708a5c60a9c5ec4da121558f1bc9f066df4e624eae075f84fe918d212ea6f1def79a0e2359239'
